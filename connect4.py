993ce2cd48d384d64ad38fe18452b7d07e7ada9c
Nihaa05 <148804343+Nihaa05@users.noreply.github.com>
2024-12-07 22:12:56 -0600

    Add files via upload

---
+++ connect4.py
@@ -0,0 +1,45 @@
+import numpy as np
+
+ROW_COUNT =6
+COLUMN_COUNT = 7
+
+def create_board():
+    board = np.zeros((6,7))
+    return board
+
+def drop_piece():
+    pass
+
+def is_valid_location(board, col):
+    return board[5][col] == 0
+
+def get_next_open_row(board, col):
+    for r in range(ROW_COUNT):
+        if board[r][col] == 0:
+            return r
+        
+    
+
+
+
+board = create_board()
+print(board)
+game_over = False
+turn = 0
+
+
+while not game_over:
+    # ask for player 1 input
+    if turn == 0:
+        col = int(input("Player 1 Make your selection (0-6):"))
+
+        #print(selection)
+
+    # ask for player 2 input
+    else:
+        col = int(input("Player 2 Make your selection (0-6):"))
+
+    turn += 1
+    turn = turn % 2
+      
+        
