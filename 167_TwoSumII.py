ff599a2968d50311a7b538aa38da76ccc0bd9e99
Nihaa05 <148804343+Nihaa05@users.noreply.github.com>
2025-05-01 09:52:43 -0500

    Add files via upload

---
+++ 167_TwoSumII.py
@@ -0,0 +1,13 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        left =0
+        right = len(numbers) -1
+        while left<right:
+            total = numbers[left] + numbers[right]
+
+            if total == target:
+                return [left+1, right+1] #1-based indexing
+            elif total < target:
+                left += 1
+            else:
+                right -= 1
